f2866b3ec4d8eefd105dfcb91704c302390a6edc
Saad Masood <me@itssaad.com>
2015-05-16 23:39:47 +0500

    init commit

---
+++ lib/c_sync/version.rb
@@ -0,0 +1,3 @@
+module CSync
+  VERSION = "0.0.1"
+end
---
+++ test/c_sync_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CSyncTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, CSync
+  end
+end
